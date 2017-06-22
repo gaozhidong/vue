8e1d4b248cbf06a824839c31b77b803134a4000b
gaozhidong <1011284295@qq.com>
2017-06-22 11:00:40 +0800

    修改 assetsPublicPath

--- resumer/src/App.vue
+++ resumer/src/App.vue
@@ -1,63 +1,75 @@
 <template>
-	<div class=page>
-	
-		<header>
-			<Topbar/>
-		</header>
-	
-		<main>
-			<ResumeEditor/>
-			<ResumePreview/>
-		</main>
-	
-	</div>
+  <div class=page>
+    <header>
+      <Topbar/>
+    </header>
+    <main>
+      <ResumeEditor/>
+      <ResumePreview/>
+    </main>
+  </div>
 </template>
 
 <script>
-import './assets/reset.css'
-import 'normalize.css/normalize.css'
+  import 'normalize.css/normalize.css'
+  import './assets/reset.css'
 
-import Topbar from './components/Topbar'
-import ResumeEditor from './components/ResumeEditor'
-import ResumePreview from './components/ResumePreview'
-console.log(Topbar)
-export default {
-	name: 'app',
-	data: function () {
-		return {
-			text: '你好'
-		}
-	},
-	components: { Topbar, ResumeEditor, ResumePreview }
-}
+  import Topbar from './components/Topbar'
+  import ResumeEditor from './components/ResumeEditor'
+  import ResumePreview from './components/ResumePreview'
+  import icons from './assets/icons'
+
+  export default {
+    name: 'app',
+    data: function () {
+      return {
+        text: '你好'
+      }
+    },
+    components: { Topbar, ResumeEditor, ResumePreview },
+    created() {
+      document.body.insertAdjacentHTML('afterbegin', icons) //
+    }
+  }
 </script>
 
-<style>
-.page {
-	height: 100vh;
-	display: flex;
-	flex-direction: column;
-}
+<style lang="scss">
+  .page{
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    background: #EAEBEC;
+    >main{
+      flex-grow: 1;  
+    }
+    >main{
+      min-width: 1024px;
+      max-width: 1440px;
+      margin-top: 16px;
+      margin-bottom: 16px;
+      display: flex;
+      justify-content: space-between;
+      padding: 0 16px;
+      width: 100%; /* 试试不加这句会怎样 */
+      align-self: center;
+    }
+  }
 
-.page>main {
-	flex-grow: 1;
-}
 
-.page>main {
-	min-width: 1024px;
-	max-width: 1440px;
-	margin: 0;
-	display: flex;
-	justify-content: space-around;
-}
-
-#resumeEditor {
-	width: 35%;
-	background: #444;
-}
-
-#resumePreview {
-	width: 61.66667%;
-	background: #777;
-}
+  #resumeEditor{
+    width: 35%;
+    background: #444;
+  }
+  #resumePreview{
+    flex-grow: 1;
+    margin-left: 16px;
+    background: #777;
+  }
+  svg.icon{
+    height: 1em;
+    width: 1em;
+    fill: currentColor;
+    vertical-align: -0.1em;
+    font-size:16px;
+  }
 </style>
--- resumer/src/router/index.js
+++ resumer/src/router/index.js
@@ -3,6 +3,8 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
+import Hello from 'components/Hello'
+
 export default new Router({
   routes: [
     {
