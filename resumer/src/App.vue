8b564b66c01988fc3aa291cd47793c938b9bbe6c
gaozhidong <1011284295@qq.com>
2017-06-23 10:12:03 +0800

    add Topbar, ResumePreview, and ResumeEditor

--- resumer/src/App.vue
+++ resumer/src/App.vue
@@ -1,20 +1,32 @@
 <template>
   <div>
-    <p>{{text}}</p>
+    <Topbar/>
+    <ResumeEditor/>
+    <ResumePreview/>
   </div>
 </template>
 
 <script>
+
+import Topbar from './components/Topbar'
+import ResumeEditor from './components/ResumeEditor'
+import ResumePreview from './components/ResumePreview'
+
+
 export default {
   name: 'app',
-  data: function(){
+  data: function () {
     return {
       text: '你好'
     }
-  }
+  },
+  components: { Topbar, ResumeEditor, ResumePreview }
+
 }
 </script>
 
 <style>
-  p{color: red;}
+p {
+  color: red;
+}
 </style>
