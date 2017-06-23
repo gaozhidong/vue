369d372bcffe48e89df9adce01796cdd125b33c6
gaozhidong <1011284295@qq.com>
2017-06-23 10:17:44 +0800

    使用 flex 布局

--- resumer/src/App.vue
+++ resumer/src/App.vue
@@ -1,12 +1,19 @@
 <template>
-  <div>
-    <Topbar/>
-    <ResumeEditor/>
-    <ResumePreview/>
+  <div class=page>
+    <header>
+      <Topbar/>
+    </header>
+    <main>
+      <ResumeEditor/>
+      <ResumePreview/>
+    </main>
   </div>
 </template>
 
 <script>
+
+import './assets/reset.css'
+import 'normalize.css/normalize.css'
 
 import Topbar from './components/Topbar'
 import ResumeEditor from './components/ResumeEditor'
@@ -26,7 +33,27 @@ export default {
 </script>
 
 <style>
-p {
-  color: red;
+.page {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+.page>main {
+  flex-grow: 1;
+}
+.page>main {
+  min-width: 1024px;
+  max-width: 1440px;
+  margin: 0;
+  display: flex;
+  justify-content: space-around;
+}
+#resumeEditor {
+  width: 35%;
+  background: #444;
+}
+#resumePreview {
+  width: 61.66667%;
+  background: #777;
 }
 </style>
