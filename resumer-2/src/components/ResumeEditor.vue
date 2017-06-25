724c25d851b2a1e4f8ca86814d05113e23798087
gaozhidong <1011284295@qq.com>
2017-06-25 16:34:44 +0800

    change README

--- resumer-2/src/components/ResumeEditor.vue
+++ resumer-2/src/components/ResumeEditor.vue
@@ -11,7 +11,16 @@
     </nav>
     <ol class="panels">
       <li v-for="item in resume.config" v-show="item.field === selected">
-        <div class="resumeField" v-for="(value,key) in resume[item.field]">
+        <div v-if="resume[item.field] instanceof Array">
+          <div class="subitem" v-for="subitem in resume[item.field]">
+            <div class="resumeField" v-for="(value,key) in subitem">
+              <label> {{key}} </label>
+              <input type="text" :value="value">
+            </div>
+            <hr>
+          </div>
+        </div>
+        <div v-else class="resumeField" v-for="(value,key) in resume[item.field]">
           <label> {{key}} </label>
           <input type="text" v-model="resume[item.field][key]">
         </div>
--- resumer/README.md
+++ resumer/README.md
@@ -14,15 +14,17 @@ npm run dev
 # build for production with minification
 npm run build
 
-# build for production and view the bundle analyzer report
-npm run build --report
-
-# run unit tests
-npm run unit
-
-# run all tests
-npm test
 ```
+## 项目分为三个组件 分别是：
+    <Topbar/>
+    <ResumeEditor/>
+    <ResumePreview/>
+
+#### 完成程度：页面分为三个大模块
+预览地址： <https://gaozhidong.github.io/vue/resumer/dist/>
+
+
+
 
 遇到的问题 
 如果打包后路径出错  把 config下的index.js  assetsPublicPath :'/'  改为 assetsPublicPath :'./'
