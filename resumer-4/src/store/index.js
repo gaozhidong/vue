cd1a99b509f7a5667b5fae7c501c3b2a34b2698e
gaozhidong <1011284295@qq.com>
2017-06-26 20:28:50 +0800

    使所有字段都可以编辑

--- resumer-4/src/store/index.js
+++ resumer-4/src/store/index.js
@@ -1,5 +1,6 @@
 import Vuex from 'vuex'
 import Vue from 'vue' // 思考：在多个文件 import vue ，会怎样
+import objectPath from "object-path"
 
 Vue.use(Vuex) // 不写这句话浏览器控制台就会报错，于是我就写了
 
@@ -95,11 +96,10 @@ export default new Vuex.Store({
       state.selected = payload // 关于 payload 看这里 http://vuex.vuejs.org/zh-cn/mutations.html#提交载荷（payload）
     },
     updateResume(state, {
-      field,
-      subfield,
+      path,
       value
     }) {
-      state.resume[field][subfield] = value
+      objectPath.set(state.resume, path, value)
     }
   }
 })
