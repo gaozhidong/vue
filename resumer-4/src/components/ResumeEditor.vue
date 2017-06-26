16909cd7c1b64f2ee068d740484ef367a4c0d98b
gaozhidong <1011284295@qq.com>
2017-06-26 20:22:05 +0800

    使用Vuex 思想重构

--- resumer-4/src/components/ResumeEditor.vue
+++ resumer-4/src/components/ResumeEditor.vue
@@ -22,7 +22,7 @@
         </div>
         <div v-else class="resumeField" v-for="(value,key) in resume[item.field]">
           <label> {{key}} </label>
-          <input type="text" v-model="resume[item.field][key]">
+          <input type="text" :value="value" @input="changeResumeField(item.field, key, $event.target.value)">
         </div>
       </li>
     </ol>
@@ -30,82 +30,92 @@
 </template>
 
 <script>
-  export default {
-    name: 'ResumeEditor',
-    computed: {
-      selected:{
-        get(){
-          return this.$store.state.selected
-        },
-        set(value){
-          return this.$store.commit('switchTab', value)
-        }
+export default {
+  name: 'ResumeEditor',
+  computed: {
+    selected: {
+      get() {
+        return this.$store.state.selected
       },
-      resume (){
-        return this.$store.state.resume
+      set(value) {
+        return this.$store.commit('switchTab', value)
       }
     },
-    methods: {
+    resume() {
+      return this.$store.state.resume
+    }
+  },
+  methods: {
+    changeResumeField(field, subfield, value) {
+      this.$store.commit('updateResume', {
+        field,
+        subfield,
+        value
+      })
     }
   }
+}
 </script>
 
 <style lang="scss" scoped>
-  #resumeEditor{
-    background:#ffffff;
-    box-shadow:0 1px 3px 0 rgba(0,0,0,0.25);
-    display: flex;
-    flex-direction: row;
-    overflow: auto;
-    > nav{
-      width: 80px;
-      background: black;
-      color: white;
-      > ol {
-        > li{
-          height: 48px;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          margin-top: 16px;
-          margin-bottom: 16px;
-          &.active{
-            background: white;
-            color: black;
-          }
+#resumeEditor {
+  background: #ffffff;
+  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.25);
+  display: flex;
+  flex-direction: row;
+  overflow: auto;
+  >nav {
+    width: 80px;
+    background: black;
+    color: white;
+    >ol {
+      >li {
+        height: 48px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-top: 16px;
+        margin-bottom: 16px;
+        &.active {
+          background: white;
+          color: black;
         }
       }
     }
-    > .panels{
-      flex-grow: 1;
-      > li {
-        padding: 24px;
-      }
-    }
-    svg.icon{
-      width: 24px; // 原设计稿 32px 不好看，改成 24px
-      height: 24px;
+  }
+  >.panels {
+    flex-grow: 1;
+    >li {
+      padding: 24px;
     }
   }
-  ol{
-    list-style: none;
+  svg.icon {
+    width: 24px; // 原设计稿 32px 不好看，改成 24px
+    height: 24px;
   }
-  .resumeField{
-    > label{
-      display: block;
-    }
-    input[type=text]{
-      margin: 16px 0;
-      border: 1px solid #ddd;
-      box-shadow:inset 0 1px 3px 0 rgba(0,0,0,0.25);
-      width: 100%;
-      height: 40px;
-      padding: 0 8px;
-    }
+}
+
+ol {
+  list-style: none;
+}
+
+.resumeField {
+  >label {
+    display: block;
   }
-  hr{
-    border: none;
-    border-top: 1px solid #ddd;
-    margin: 24px 0;
+  input[type=text] {
+    margin: 16px 0;
+    border: 1px solid #ddd;
+    box-shadow: inset 0 1px 3px 0 rgba(0, 0, 0, 0.25);
+    width: 100%;
+    height: 40px;
+    padding: 0 8px;
   }
+}
+
+hr {
+  border: none;
+  border-top: 1px solid #ddd;
+  margin: 24px 0;
+}
 </style>
--- resumer-4/src/store/index.js
+++ resumer-4/src/store/index.js
@@ -7,13 +7,30 @@ export default new Vuex.Store({
   state: {
     selected: 'profile',
     resume: {
-      config: [
-        { field: 'profile', icon: 'id' },
-        { field: 'workHistory', icon: 'work' },
-        { field: 'education', icon: 'book' },
-        { field: 'projects', icon: 'heart' },
-        { field: 'awards', icon: 'cup' },
-        { field: 'contacts', icon: 'phone' },
+      config: [{
+          field: 'profile',
+          icon: 'id'
+        },
+        {
+          field: 'workHistory',
+          icon: 'work'
+        },
+        {
+          field: 'education',
+          icon: 'book'
+        },
+        {
+          field: 'projects',
+          icon: 'heart'
+        },
+        {
+          field: 'awards',
+          icon: 'cup'
+        },
+        {
+          field: 'contacts',
+          icon: 'phone'
+        },
       ],
       profile: {
         name: '高某某',
@@ -21,39 +38,68 @@ export default new Vuex.Store({
         title: '自由职业',
         birthday: '1990-01-01'
       },
-      workHistory: [
-        {
-          company: '鸡飞狗跳公司', content: `公司总部设在XXXX区，先后在北京、上海成立分公司。专注于移动XXX领域，主打产品XXXXX，它将资讯、报纸、杂志、图片、微信等众多内容，按照用户意愿聚合到一起，实现深度个性化 定制。
+      workHistory: [{
+          company: '鸡飞狗跳公司',
+          content: `公司总部设在XXXX区，先后在北京、上海成立分公司。专注于移动XXX领域，主打产品XXXXX，它将资讯、报纸、杂志、图片、微信等众多内容，按照用户意愿聚合到一起，实现深度个性化 定制。
 我的主要工作如下:
 1. 完成既定产品需求。
 2. 修复 bug。`
         },
-        { company: '狗急跳墙责任有限公司', content: `公司总部设在XXXX区，先后在北京、上海成立分公司。专注于移动XXX领域，主打产品XXXXX，它将资讯、报纸、杂志、图片、微信等众多内容，按照用户意愿聚合到一起，实现深度个性化 定制。
+        {
+          company: '狗急跳墙责任有限公司',
+          content: `公司总部设在XXXX区，先后在北京、上海成立分公司。专注于移动XXX领域，主打产品XXXXX，它将资讯、报纸、杂志、图片、微信等众多内容，按照用户意愿聚合到一起，实现深度个性化 定制。
 我的主要工作如下:
 1. 完成既定产品需求。
-2. 修复 bug` },
+2. 修复 bug`
+        },
       ],
-      education: [
-        { school: '黄志诚警官大学', content: '本科' },
-        { school: '韩琛古惑仔高中'},
+      education: [{
+          school: '黄志诚警官大学',
+          content: '本科'
+        },
+        {
+          school: '韩琛古惑仔高中'
+        },
       ],
-      projects: [
-        { name: 'project A', content: '文字' },
-        { name: 'project B', content: '文字' },
+      projects: [{
+          name: 'project A',
+          content: '文字'
+        },
+        {
+          name: 'project B',
+          content: '文字'
+        },
       ],
-      awards: [
-        { name: 'awards A', content: '文字' },
-        { name: 'awards B', content: '文字' },
+      awards: [{
+          name: 'awards A',
+          content: '文字'
+        },
+        {
+          name: 'awards B',
+          content: '文字'
+        },
       ],
-      contacts: [
-        { contact: 'phone', content: '13812345678' },
-        { contact: 'qq', content: '12345678' },
+      contacts: [{
+          contact: 'phone',
+          content: '13812345678'
+        },
+        {
+          contact: 'qq',
+          content: '12345678'
+        },
       ],
     }
   },
   mutations: {
     switchTab(state, payload) {
       state.selected = payload // 关于 payload 看这里 http://vuex.vuejs.org/zh-cn/mutations.html#提交载荷（payload）
+    },
+    updateResume(state, {
+      field,
+      subfield,
+      value
+    }) {
+      state.resume[field][subfield] = value
     }
   }
 })
