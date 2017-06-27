f1bc5ecb3183f027e90400ec9c687c1e587d90a0
gaozhidong <1011284295@qq.com>
2017-06-27 08:55:22 +0800

    显示用户名和登出

--- resumer-4/src/components/Topbar.vue
+++ resumer-4/src/components/Topbar.vue
@@ -3,18 +3,21 @@
     <div class="wrapper">
       <span class="logo">Resumer</span>
   
-      <div class="actions">
-        <span>{{user}}</span>
+      <div v-if="logined" class="userActions">
+        <span>你好，{{user.username}}</span>
+        <a class="button" href="#">登出</a>
+      </div>
+      <div v-else class="userActions">
         <a class="button primary" href="#" @click.prevent="signUpDialogVisible = true">注册</a>
         <MyDialog title="注册" :visible="signUpDialogVisible" @close="signUpDialogVisible = false">
           <SignUpForm @success="login($event)" />
         </MyDialog>
         <a class="button" href="#">登录</a>
-        <button class="button primary">保存</button>
-        <button class="button">预览</button>
-      </div>
-  
+      </div>     
+      <button class="button primary">保存</button>
+      <button class="button">预览</button>
     </div>
+  
   </div>
 </template>
 
@@ -31,6 +34,9 @@ export default {
   computed: {
     user() {
       return this.$store.state.user
+    },
+    logined(){
+      return this.user.id
     }
   },
   components: {
@@ -91,5 +97,10 @@ export default {
   }
 }
 
-.actions>a {}
+.actions{
+  display: flex;
+  .userActions{
+    margin-right: 3em;
+  }
+}
 </style>
