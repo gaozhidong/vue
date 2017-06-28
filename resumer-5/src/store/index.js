2247a87e78211d42e90a121a71cbd0f53bb24719
gaozhidong <1011284295@qq.com>
2017-06-28 10:03:21 +0800

    将config和resume 分开

--- resumer-5/src/store/index.js
+++ resumer-5/src/store/index.js
@@ -2,7 +2,9 @@ import Vuex from 'vuex'
 import Vue from 'vue' // 思考：在多个文件 import vue ，会怎样
 import objectPath from "object-path"
 
+
 Vue.use(Vuex) // 不写这句话浏览器控制台就会报错，于是我就写了
+
 export default new Vuex.Store({
   state: {
     selected: 'profile',
@@ -10,54 +12,62 @@ export default new Vuex.Store({
       id: '',
       username: ''
     },
+    resumeConfig: [{
+        field: 'profile',
+        icon: 'id',
+        keys: ['name', 'city', 'title', 'birthday']
+      },
+      {
+        field: 'workHistory',
+        icon: 'work',
+        type: 'array',
+        keys: ['company', 'details']
+      },
+      {
+        field: 'education',
+        icon: 'book',
+        type: 'array',
+        keys: ['school', 'details']
+      },
+      {
+        field: 'projects',
+        icon: 'heart',
+        type: 'array',
+        keys: ['name', 'details']
+      },
+      {
+        field: 'awards',
+        icon: 'cup',
+        type: 'array',
+        keys: ['name', 'details']
+      },
+      {
+        field: 'contacts',
+        icon: 'phone',
+        type: 'array',
+        keys: ['contact', 'content']
+      },
+    ],
     resume: {
-      config: [{
-          field: 'profile',
-          icon: 'id',
-          keys: ['name', 'city', 'title', 'birthday']
-        },
-        {
-          field: 'workHistory',
-          icon: 'work',
-          type: 'array',
-          keys: ['company', 'details']
-        },
-        {
-          field: 'education',
-          icon: 'book',
-          type: 'array',
-          keys: ['school', 'details']
-        },
-        {
-          field: 'projects',
-          icon: 'heart',
-          type: 'array',
-          keys: ['name', 'details']
-        },
-        {
-          field: 'awards',
-          icon: 'cup',
-          type: 'array',
-          keys: ['name', 'details']
-        },
-        {
-          field: 'contacts',
-          icon: 'phone',
-          type: 'array',
-          keys: ['contact', 'content']
-        },
-      ],
-      profile: {},
-      workHistory: [],
-      education: [],
-      projects: [],
-      awards: [],
-      contacts: [],
 
     }
   },
   mutations: {
     initState(state, payload) {
+
+      state.resumeConfig.map((item) => {
+        if (item.type === 'array') {
+          //state.resume[item.field] = [] // 这样写 Vue 无法监听属性变化
+          Vue.set(state.resume, item.field, [])
+        } else {
+          //state.resume[item.field] = {} // 这样写 Vue 无法监听属性变化
+          Vue.set(state.resume, item.field, {})
+          item.keys.map((key) => {
+            //state.resume[item.field][key] = '' // 这样写 Vue 无法监听属性变化
+            Vue.set(state.resume[item.field], key, '')
+          })
+        }
+      })
       Object.assign(state, payload)
     },
     switchTab(state, payload) {
@@ -77,5 +87,6 @@ export default new Vuex.Store({
     removeUser(state) {
       state.user.id = null
     }
+
   }
 })
