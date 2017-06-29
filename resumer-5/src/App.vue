947e028a07d0bb602e95512e4c26127c6facf186
gaozhidong <1011284295@qq.com>
2017-06-29 08:48:06 +0800

    将 button 的样式提取到 App，公用

--- resumer-5/src/App.vue
+++ resumer-5/src/App.vue
@@ -1,87 +1,111 @@
 <template>
-   <div>
-     <div class=page>
-       <header>
-         <Topbar/>
-       </header>
-       <main>
-         <ResumeEditor/>
-         <ResumePreview/>
-       </main>
-     </div>
-   </div>
- </template>
+  <div>
+    <div class=page>
+      <header>
+        <Topbar/>
+      </header>
+      <main>
+        <ResumeEditor/>
+        <ResumePreview/>
+      </main>
+    </div>
+  </div>
+</template>
  
  <script>
-   import 'normalize.css/normalize.css'
-   import './assets/reset.css'
- 
-   import Topbar from './components/Topbar'
-   import ResumeEditor from './components/ResumeEditor'
-   import ResumePreview from './components/ResumePreview'
-   import icons from './assets/icons'
- 
-   import store from './store/index'
-   import AV from './lib/leancloud'
-   import getAVUser from './lib/getAVUser'
- 
-   export default {
-     name: 'app',
-     store,
-     components: { Topbar, ResumeEditor, ResumePreview},
-     created() {
-        document.body.insertAdjacentHTML('afterbegin', icons) //
-        let state = localStorage.getItem('state')
-        if(state){
+import 'normalize.css/normalize.css'
+import './assets/reset.css'
 
-       state = JSON.parse(state) 
-        }
+import Topbar from './components/Topbar'
+import ResumeEditor from './components/ResumeEditor'
+import ResumePreview from './components/ResumePreview'
+import icons from './assets/icons'
 
-     // 先临时关闭一下，以免影响我开发
-     this.$store.commit('initState', {})
-        this.$store.commit('setUser', getAVUser())
-  
-      }
-   }
- </script>
+import store from './store/index'
+import AV from './lib/leancloud'
+import getAVUser from './lib/getAVUser'
+
+export default {
+  name: 'app',
+  store,
+  components: { Topbar, ResumeEditor, ResumePreview },
+  created() {
+    document.body.insertAdjacentHTML('afterbegin', icons) //
+    let state = localStorage.getItem('state')
+    if (state) {
+
+      state = JSON.parse(state)
+    }
+
+    // 先临时关闭一下，以免影响我开发
+    this.$store.commit('initState', {})
+    this.$store.commit('setUser', getAVUser())
+
+  }
+}
+</script>
  
  <style lang="scss">
-   .page{
-     height: 100vh;
-     display: flex;
-     flex-direction: column;
-     background: #EAEBEC;
-     >main{
-       flex-grow: 1;  
-     }
-     >main{
-       min-width: 1024px;
-       max-width: 1440px;
-       margin-top: 16px;
-       margin-bottom: 16px;
-       display: flex;
-       justify-content: space-between;
-       padding: 0 16px;
-       width: 100%; /* 试试不加这句会怎样 */
-       align-self: center;
-     }
-   }
- 
- 
-   #resumeEditor{
-     min-width: 35%;
-     background: #444;
-   }
-   #resumePreview{
-     flex-grow: 1;
-     margin-left: 16px;
-     background: #777;
-   }
-   svg.icon{
-     height: 1em;
-     width: 1em;
-     fill: currentColor;
-     vertical-align: -0.1em;
-     font-size:16px;
-   }
- </style>
+.page {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  background: #EAEBEC;
+  >main {
+    flex-grow: 1;
+  }
+  >main {
+    min-width: 1024px;
+    max-width: 1440px;
+    margin-top: 16px;
+    margin-bottom: 16px;
+    display: flex;
+    justify-content: space-between;
+    padding: 0 16px;
+    width: 100%;
+    /* 试试不加这句会怎样 */
+    align-self: center;
+  }
+}
+
+
+#resumeEditor {
+  min-width: 35%;
+  background: #444;
+}
+
+#resumePreview {
+  flex-grow: 1;
+  margin-left: 16px;
+  background: #777;
+}
+
+svg.icon {
+  height: 1em;
+  width: 1em;
+  fill: currentColor;
+  vertical-align: -0.1em;
+  font-size: 16px;
+}
+
+.button {
+  width: 72px;
+  height: 32px;
+  border: none;
+  cursor: pointer;
+  font-size: 18px;
+  color: #222;
+  text-decoration: none;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
+  &:hover {
+    box-shadow: 1px 1px 1px hsla(0, 0, 0, 0.50);
+  }
+  &.primary {
+    background: #02af5f;
+    color: white;
+  }
+}
+</style>
